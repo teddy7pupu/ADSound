f6a7366a9d5c094ab89e4cca193602b8b8ce50a2
teddy7pupu <cloveradd@gmail.com>
2018-03-22 16:09:52 +0800

    @Add LoginViewController @Reason Page layout

--- ADSound/Controller/MainViewController.swift
+++ ADSound/Controller/MainViewController.swift
@@ -13,7 +13,17 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.isNavigationBarHidden = false
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
